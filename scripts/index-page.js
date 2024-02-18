7e5a5dadb3ab60d4c93740d7645b96d7d61905de
erioluwa66 <erioluwa66@gmail.com>
2024-02-18 13:09:49 -0500

    comment added to the top now working

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -2,205 +2,77 @@ import {bandSiteApi} from "./band-site-api.js";
 const api = new bandSiteApi();
 
 // Getting comments
+// Getting comments
 const getComment = async () => {
     try {
         const comments = await api.getComments();
         console.log("Comments:", comments);
-        displayComments(comments);
+        displayComment(comments);
     } catch (error) {
         console.error("Error getting comments:", error);
     }
 };
 getComment();
 
-// //Default comments array
-// let comments = [
-//   {
-//     name: "Victor Pinto",
-//     date: "11/02/2023",
-//     comment:
-//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-//   },
-//   {
-//     name: "Christina Cabrera",
-//     date: "10/28/2023",
-//     comment:
-//       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-//   },
-//   {
-//     name: "Isaac Tadesse",
-//     date: "10/20/2023",
-//     comment:
-//       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-//   }
-// ];
-
-
-function displayComments(arr) {
-  let commentContainer = document.querySelector(".comment__section");
-
-  for (let i = 0; i < arr.length; i++) {
-    //date setting
-    let m = new Date(arr[i]["timestamp"]);
-    let dateString =
-      m.getUTCMonth() + 1 + "/" + m.getUTCDate() + "/" + m.getUTCFullYear();
-
-    // default comment div
-    let defaultContainer = document.createElement("div");
-    defaultContainer.classList.add("comment__default");
-    commentContainer.appendChild(defaultContainer);
-
-    //photo container
-    let photoContainer = document.createElement("div");
-    photoContainer.classList.add("comment__photo-box-one");
-    defaultContainer.appendChild(photoContainer);
-
-    // default comments
-    let headerContainer = document.createElement("div");
-    headerContainer.classList.add("comment__header");
-    defaultContainer.appendChild(headerContainer);
-
-    //photo
-    let photo = document.createElement("div");
-    photo.classList.add("comment__header--photo-one");
-    photoContainer.appendChild(photo);
-
-    //header name
-    let name = document.createElement("h2");
-    name.classList.add("comment__header--name");
-    name.innerText = arr[i]["name"];
-    headerContainer.appendChild(name);
-
-    //header date
-    let date = document.createElement("h3");
-    date.classList.add("comment__header--date");
-    date.innerText = dateString;
-    headerContainer.appendChild(date);
-
-    //comment box
-    let textContainer = document.createElement("div");
-    textContainer.classList.add("comment__text-box-main");
-    defaultContainer.appendChild(textContainer);
-
-    //actualcomment
-    let comment = document.createElement("p");
-    comment.classList.add("comment__text-box-main--comment");
-    comment.innerText = arr[i]["comment"];
-    textContainer.appendChild(comment);
-  }
-}
-
-
-// const addComment = async () => {
-//     const comment = {
-//         "name": Mariam,
-//         "comment": "God will do it."
-//     };
-
-//     try {
-//         const postedComment = await api.postComment(comment);
-//         console.log("Posted Comment:", postedComment);
-//     } catch (error) {
-//         console.error("Error posting comment:", error);
-//     }
-// };
-//addComment();
-
-// Event listener setup
-const form = document.querySelector(".comment__input-box");
-form.addEventListener("submit", async (submitEvent) => {
-  // Prevents page reloading after clicking the submit button
-  submitEvent.preventDefault();
-  
-  try {
-    // Post comments
-    const response = await axios.post(
-      "https://project-1-api.herokuapp.com/comments?api_key=ce0a83d6-f938-4f6d-84da-9d270c0130d7", {
-      name: submitEvent.target.name.value,
-      comment: submitEvent.target.comment.value
-    });
+   //Displaying Comments invoked from Fetch Comments
+    function displayComment(comments) {
     
-    // Handle the response as needed (e.g., display a success message)
-    console.log("Comment posted successfully:", response.data);
-    
-    // Clear the form inputs after posting the comment
-    submitEvent.target.name.value = '';
-    submitEvent.target.comment.value = '';
-  } catch (error) {
-    console.error("Error posting comment:", error);
-  }
-});
+        const commentsSection = document.querySelector(".comments");
+        commentsSection.innerHTML = "";
+        //looping through each comment
+        comments.forEach((comment) => {
 
-// function displayComments(comments) {
-//     const commentList = document.getElementById('comment-list');
-//     commentList.innerHTML = ''; // Clear previous comments
+            const commentContainer = document.createElement("div");
+            const avatarContainer = document.createElement("div");
+            const userDetail = document.createElement("div");
+            const nameContainer = document.createElement("div");
+            const dateContainer = document.createElement("div");
+            const name = document.createElement("p");
+            const date = document.createElement("p");
+            const commentDes = document.createElement("div");
+            const userComment = document.createElement("div");
+            name.innerText = comment.name;
+            date.innerText = (new Date(comment.timestamp).toLocaleDateString("en-US"));
+            commentDes.innerText = comment.comment;
 
-// //     // Display comments, starting with the newest ones
-// //     for (let i = comments.length - 1; i >= 0; i--) {
-// //         const comment = comments[i];
-// //         const commentItem = document.createElement('div');
-// //         commentItem.classList.add('comment-item');
-// //         commentItem.innerHTML = `
-// //             <div class="comment-author">${comment.name}</div>
-// //             <div class="comment-text">${comment.comment}</div>
-// //         `;
-// //         // Prepend the comment to the beginning of the list
-// //         commentList.prepend(commentItem);
-//     }
-// }
-//   let commentContainer = document.querySelector(".comment__section");
- 
-//   let defaultContainer = document.createElement("div");
-//   defaultContainer.classList.add("comment__default");
-//   commentContainer.appendChild(defaultContainer);
+            avatarContainer.classList.add("comments__avatar-cont");
+            userDetail.classList.add("comments__user-detail");
+            name.classList.add("comments__name");
+            date.classList.add("comments__date");
+            commentDes.classList.add("comments__comment-des");
+            userComment.classList.add("comments__user-cont");
+            commentContainer.classList.add("comments__comment-cont");
 
+            nameContainer.appendChild(name);
+            dateContainer.appendChild(date);
+            userDetail.append(nameContainer, dateContainer);
+            userComment.append(userDetail, commentDes);
+            commentContainer.append(avatarContainer, userComment);
+            commentsSection.appendChild(commentContainer);
+        });   
 
-//   let photoContainer = document.createElement("div");
-//   photoContainer.classList.add("comment__photo-box-one");
-//   defaultContainer.appendChild(photoContainer);
+        const firstChild = commentsSection.firstElementChild;
+        const lastChild = commentsSection.lastElementChild;
 
+        firstChild.classList.add("comments__comment-cont--fnode");
+        lastChild.classList.add("comments__comment-cont--lnode");
 
-//   let headerContainer = document.createElement("div");
-//   headerContainer.classList.add("comment__header");
-//   defaultContainer.appendChild(headerContainer);
+    };
 
+   //Event Listener Setup;
+    function listen(){
+        const form = document.querySelector(".conv__form");
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            axios.post( "https://project-1-api.herokuapp.com/comments?api_key=ce0a83d6-f938-4f6d-84da-9d270c0130d7", {
+                name: e.target.name.value,
+                comment: e.target.comment.value
+            }).then((res) =>{
+                getComment();
+            }).catch((error) =>{
+                console.log(error);
+            })
+            form.reset()
+        });
 
-//   let photo = document.createElement("div");
-//   photo.classList.add("comment__header--photo-one");
-//   photoContainer.appendChild(photo);
-
-
-//   let name = document.createElement("h2");
-//   name.classList.add("comment__header--name");
-//   name.innerText = newComment.name;
-//   headerContainer.appendChild(name);
-
-//   // //date
-//   // let dd = String(today.getDate()).padStart(2, "0");
-//   // let mm = String(today.getMonth() + 1).padStart(2, "0");
-//   // let yyyy = today.getFullYear();
-
-//   // today = mm + "/" + dd + "/" + yyyy;
-
-//   //date
-//   let date = document.createElement("h3");
-//   date.innerText = today;
-//   date.classList.add("comment__header--date");
-//   headerContainer.appendChild(date);
-
-//   let textContainer = document.createElement("div");
-//   textContainer.classList.add("comment__text-box-main");
-//   defaultContainer.appendChild(textContainer);
-
-
-//   let comment = document.createElement("p");
-//   comment.classList.add("comment__text-box-main--comment");
-//   comment.innerText = newComment.comment;
-//   textContainer.appendChild(comment);
-
-//   let top = document.querySelector(".comment__section");
-//   top.insertBefore(defaultContainer, top.childNodes[0]);
-
-//   let clearInput = document.querySelector(".comment__input-box");
-//   clearInput.reset();
-// });
+    }
